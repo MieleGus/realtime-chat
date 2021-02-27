b1aa57dc92714dbc0986600c898c13f859969c2c
Gustavo Fraga <gustavohenriquemiele@live.com>
2021-02-27 12:32:33 -0300

    Input tooltip inserted

--- frontend/src/components/Input/index.js
+++ frontend/src/components/Input/index.js
@@ -3,7 +3,8 @@ import { IconBaseProps}  from 'react-icons';
 import { FiAlertCircle }  from 'react-icons/fi';
 
 import { useField } from '@unform/core'
-import { Container } from './styles';
+import { Container, Error } from './styles';
+import Tooltip from '../Tooltip';
 
 const Input = ({name, icon: Icon, ...rest }) => {
     const inputRef = useRef(null);
@@ -40,6 +41,11 @@ const Input = ({name, icon: Icon, ...rest }) => {
                 ref={inputRef}
                 {...rest}
             />
+            {error &&
+                <Error title={error}>
+                    <FiAlertCircle color="#c53030" size={20}/>
+                </Error>
+            }
            
         </Container>
     );
--- frontend/src/components/Input/styles.js
+++ frontend/src/components/Input/styles.js
@@ -1,5 +1,7 @@
 import styled, { css} from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 export const Container = styled.div`
     border-radius: 10px;
     background: #232129;
@@ -41,3 +43,18 @@ export const Container = styled.div`
         margin-right: 16px;
     }
     `;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+  span {
+    background: #c53030;
+    color: #fff;
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
